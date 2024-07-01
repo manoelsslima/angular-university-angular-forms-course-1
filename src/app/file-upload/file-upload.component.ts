00753d1ac092636f9085d25166fe95241c434a1e
Manoel Lima <manoel.lima@manaus.am.gov.br>
2024-07-01 12:46:18 -0400

    Angular FileUpload - Uploading to a Node Server using the HTTP Client

--- src/app/file-upload/file-upload.component.ts
+++ src/app/file-upload/file-upload.component.ts
@@ -1,27 +1,52 @@
-import {Component, Input} from '@angular/core';
-import { HttpClient, HttpEventType } from '@angular/common/http';
-import {catchError, finalize} from 'rxjs/operators';
-import {AbstractControl, ControlValueAccessor, NG_VALIDATORS, NG_VALUE_ACCESSOR, Validator} from '@angular/forms';
-import {noop, of} from 'rxjs';
-
+import { Component, Input } from "@angular/core";
+import { HttpClient, HttpEventType } from "@angular/common/http";
+import { catchError, finalize } from "rxjs/operators";
+import {
+  AbstractControl,
+  ControlValueAccessor,
+  NG_VALIDATORS,
+  NG_VALUE_ACCESSOR,
+  Validator,
+} from "@angular/forms";
+import { Observable, noop, of } from "rxjs";
 
 @Component({
-  selector: 'file-upload',
+  selector: "file-upload",
   templateUrl: "file-upload.component.html",
-  styleUrls: ["file-upload.component.scss"]
+  styleUrls: ["file-upload.component.scss"],
 })
 export class FileUploadComponent {
-
   @Input()
   requiredFileType: string;
 
-  fileName: string = '';
+  fileName: string = "";
+
+  fileUploadError = false;
+
+  constructor(private http: HttpClient) {}
 
   onFileSelected(event) {
     const file: File = event.target.files[0];
-    if (file) { // evita erro ao clicar em cancelar
+    if (file) {
+      // evita erro ao clicar em cancelar
+
       this.fileName = file.name;
-      console.log(this.fileName);
-    }    
+
+      const formData = new FormData();
+
+      formData.append("thumbnail", file);
+
+      this.fileUploadError = false;
+
+      this.http
+        .post("/api/thumbnail-upload", formData)
+        .pipe(
+          catchError((error) => {
+            this.fileUploadError = true;
+            return of(error);
+          })
+        )
+        .subscribe();
+    }
   }
 }
